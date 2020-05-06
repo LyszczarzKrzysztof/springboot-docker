4246b63fe4c7796b542025422e405d938f6735af
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2020-05-06 19:06:20 +0200

    Initial commit

---
+++ src/test/java/com/example/sprinbootdocker/SprinbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
